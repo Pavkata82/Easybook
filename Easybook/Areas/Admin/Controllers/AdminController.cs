1297246ee8d1ad9015ba366a4ad814e46d780dfc
Pavkata82 <pavkata1926@gmail.com>
2025-02-25 13:52:39 +0200

    Add Admin panel

---
+++ Easybook/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Easybook.Constants;
+
+namespace Easybook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = Roles.Admin)] // Restrict access to Admins only
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUsers()
+        {
+            return View();
+        }
+
+        public IActionResult ManageBookings()
+        {
+            return View();
+        }
+    }
+}
--- Easybook/Program.cs
+++ Easybook/Program.cs
@@ -91,6 +91,14 @@ namespace Easybook
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+            );
+
+
+
             app.MapRazorPages();
 
             app.Run();
