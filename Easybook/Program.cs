031f083a3cb014cd2a975a8674eccbd98f97a276
Pavkata82 <pavkata1926@gmail.com>
2025-02-25 21:24:09 +0200

    Fix admin panel url

--- Easybook/Program.cs
+++ Easybook/Program.cs
@@ -89,14 +89,13 @@ namespace Easybook
             app.UseAuthorization();
 
             app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.MapControllerRoute(
                 name: "areas",
                 pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
             );
 
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
             app.MapRazorPages();
